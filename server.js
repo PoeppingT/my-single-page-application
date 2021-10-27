c86ac430cfb0f154d3e5861e1739397287949730
PoeppingT <thomas@poepping.org>
2021-10-26 23:13:50 -0700

    working through #LatestCurrencyRates, at implementing /api/rates

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
+const { getRates } = require('./lib/fixer-service');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,36 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+// Redirect all traffic to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 // Listen for HTTP requests on port 3000 (or configured)
 app.listen(port, () => {
   console.log('listening on %d', port);
+});
+
+// Express Error handler
+const errorHandler = (err, req, res) => {
+  if (err.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range 2xx
+    res.status(403).send({ title: 'Server responded with an error', message: err.message });
+  } else if (err.request) {
+    // The request was made but no response was received
+    res.status(503).send({ title: 'Unable to communicate with server', message: err.message });
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    res.status(500).send({ title: 'An unexpected error occurred', message: err.message });
+  }
+};
+
+// Fetch Latest Currency Rates
+app.get('/api/rates', async (req, res) => {
+  try {
+    const data = await getRates();
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 });
